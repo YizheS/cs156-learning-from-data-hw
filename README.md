d7c1591a8438beda2bc81d7fa5703c6a01fd8e2c
Derek Kwan <derek.x.kwan@gmail.com>
2017-05-15 20:15:26 -0700

    adding hw2_hoeffding

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # learning-from-data-hw
 self-study work for caltech's cs156 machine learning course
 
-- hw1: basic probabiliy and percectrpon learning algorithm (coded as a class in pla.py)
+- hw1: basic probabiliy and perceptron learning algorithm (coded as a class in pla.py) **DONE**
+- hw2: testing hoeffding inequality with simulated coin flips, rest I haven't finished yet
---
+++ hw2_hoeffding.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class FairCoin:
+    def __init__(self, num_flips):
+        self.num_flips = max(1, num_flips)
+        #heads = 1, tails = 0
+        self.flips = np.random.randint(0, 2, self.num_flips)
+        self.freq_heads = np.average(self.flips)
+
+class CoinFlips:
+    def __init__(self, num_coins):
+        #num_exp = num experiments
+        self.num_coins = max(1, num_coins)
+        self.coin_flips = [FairCoin(10) for x in range(self.num_coins)]
+        self.head_freqs = np.array([x.freq_heads for x in self.coin_flips])
+        self.first_coin_freq = self.head_freqs[0]
+        self.rand_coin_freq = self.head_freqs[np.random.randint(0, self.num_coins)]
+        self.min_coin_freq = self.head_freqs.min()
+        
+def hw2_hoeffding(num_exp):
+    hw2_hexps = [CoinFlips(1000) for x in range(num_exp)]
+    nu_mins = np.array([x.min_coin_freq for x in hw2_hexps])
+    nu_firsts = np.array([x.first_coin_freq for x in hw2_hexps])
+    nu_rand = np.array([x.rand_coin_freq for x in hw2_hexps])
+    mins_avg = np.average(nu_mins)
+    firsts_avg = np.average(nu_firsts)
+    rand_avg = np.average(nu_rand)
+    print("Average number of heads:")
+    print("first coins of sims: %f" % firsts_avg)
+    print("coin w/ min freq of heads of sims: %f" %  mins_avg)
+    print("rand selection of coin of sims: %f" % rand_avg)
+    
+
+hw2_hoeffding(10000)
+
+"""
+assignment asks for 10 times the experiments, but it was running a little long so this should be fine
+
+EXAMPLE OUTPUT
+Average number of heads:
+first coins of sims: 0.498810
+coin w/ min freq of heads of sims: 0.038080
+rand selection of coin of sims: 0.501150
+"""
