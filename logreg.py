b78e56f0c1d022aec97c85d1c27c1e10207b8598
Derek Kwan <derek.x.kwan@gmail.com>
2017-06-01 05:48:15 -0700

    finish hw5

--- logreg.py
+++ logreg.py
@@ -13,16 +13,18 @@ class LogReg:
         
     def reshape_X(self, X):
         #number of examples
-        if len(X.shape) > 1:
+        if len(X.shape) > 1 and X.shape[0] >= 1:
             num_ex = X.shape[0]
             return np.c_[ np.ones(num_ex), X]
         else:
+            cur_size = X.size
             return np.r_[1, X]
 
     def risk_score(self, X):
         #should return (n, 1)
         res_X = self.reshape_X(X)
-        return np.dot(X,self.weights)
+        my_risk = np.dot(res_X,self.weights)
+        return my_risk
 
     def sigmoid(self, X):
         #theta(s) = e^s/(1+e^s)
@@ -44,11 +46,39 @@ class LogReg:
         cur_sum = np.divide(cur_sum, -1*cur_N)
         return cur_sum
 
+    def sto_gradient(self, xn, yn):
+        #stochastic gradient, should be only one example
+        res_X = self.reshape_X(xn)
+        cur_numer = np.multiply(yn, res_X)
+        cur_denom = np.add(1, np.exp(np.multiply(yn, self.risk_score(xn))))
+        return np.multiply(-1, np.divide(cur_numer, cur_denom))
+    
     def update_weights(self, X, y):
         #w(t+1) = w(t) - l_rate * gradient
         cur_grad = self.gradient(X,y)
         self.weights = np.subtract(self.weights, np.multiply(self.l_rate, cur_grad))
     
+    def sto_gd(self, X, y):
+        # a run of stochastic gradient descent
+        cur_num = X.shape[0]
+        #get indices for every row/example in X and shuffle them
+        cur_idxs = np.arange(cur_num)
+        np.random.shuffle(cur_idxs)
+        #now update weights one by one
+        for cur_idx in cur_idxs:
+            cur_grad = self.sto_gradient(X[cur_idx], y[cur_idx])
+            self.weights = np.subtract(self.weights, np.multiply(self.l_rate, cur_grad))
+
+    def ce_error(self, X, y):
+        #cross-entropy error
+        #e_in = (1/N) sum(n=1;N){ ln(1+e^(-yn*wT*xn))}
+        res_X = self.reshape_X(X)
+        cur_N = res_X.shape[0]
+        cur_val = np.log(np.add(1, np.exp(np.multiply(np.multiply(-1,y), self.risk_score(X)))))
+        #should be (n,1)
+        return np.divide(np.sum(cur_val), cur_N)
+        
+
         
 
     
